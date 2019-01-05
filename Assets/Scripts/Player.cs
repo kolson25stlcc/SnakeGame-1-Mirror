71715ddfcb6aed4036f438342e5aee4fb0caa24a
ZChristensen11 <46354226+ZChristensen11@users.noreply.github.com>
2019-01-04 19:00:29 -0600

    Added position grid for snake and food, added border

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,23 +12,62 @@ public class Player : MonoBehaviour
 
     public GameObject bodyPrefab;
 
-    public float moveDistance = 0.03f;
-    Vector2 dir = Vector2.zero;
-    Vector2 right; bool righting = false;
-    Vector2 down; bool downing = false;
-    Vector2 left; bool lefting = false;
-    Vector2 up; bool uping = false;
+    public static float moveDistance = 0.279f; // specific to the size of the snake blocks
+    Vector3 dir = Vector3.zero;
+    Vector3 right; bool righting = false;
+    Vector3 down; bool downing = false;
+    Vector3 left; bool lefting = false;
+    Vector3 up; bool uping = false;
 
-    List<Transform> tail = new List<Transform>();
+    public static List<Transform> tail = new List<Transform>();
 
     public float speed = 0.3f;
 
+
+
+    //private float[] validXLocs = {2.511f, 2.232f, 1.953f, 1.674f, 1.395f, 1.116f, 0.837f, 0.558f, 0.279f, 0,
+    //                                -2.511f, -2.232f, -1.953f, -1.674f, -1.395f, -1.116f, -0.837f, -0.558f, -0.279f};
+    //private float[] validYLocs = {3.431f, 3.152f, 2.873f, 2.315f, 2.036f, 1.757f, 1.478f, 1.199f, 0.92f, 0.641f, 0.362f, 0.083f,
+    //                                -0.196f, -0.475f, -0.754f, -1.033f, -1.312f, -1.591f, -1.87f, -2.149f, -2.428f, -2.707f, -2.986f,
+    //                                -3.265f, -3.544f, -3.823f, -4.102f, -4.381f, -4.66f};
+    private float[] validXLocs = new float[19];
+    private float[] validYLocs = new float[30];
+
+    private int xCounter = 9;
+    private int yCounter = 15;
+
+
+
     void Start()
     {
-        right = new Vector2(moveDistance, 0);
-        down = new Vector2(0, -moveDistance);
-        left = new Vector2(-moveDistance, 0);
-        up = new Vector2(0, moveDistance);
+
+        //Debug.Log("Res: " + Screen.currentResolution + " DPI: " + Screen.dpi);
+        //Renderer rend = gameObject.GetComponent<Renderer>();
+        //Debug.Log(rend.bounds.max);
+        //Debug.Log(rend.bounds.min);
+
+        float minX = -2.511f;
+        validXLocs[0] = minX;
+        for (int i = 1; i < validXLocs.Length; i++)
+        {
+            minX += moveDistance;
+            validXLocs[i] = minX;
+        }
+
+        float minY = -4.66f;
+        validYLocs[0] = minY;
+        for (int i = 1; i < validYLocs.Length; i++)
+        {
+            minY += moveDistance;
+            validYLocs[i] = minY;
+        }
+
+        transform.position = new Vector3(validXLocs[xCounter], validYLocs[yCounter]);
+
+        right = new Vector3(moveDistance, 0);
+        down = new Vector3(0, -moveDistance);
+        left = new Vector3(-moveDistance, 0);
+        up = new Vector3(0, moveDistance);
 
         InvokeRepeating("Move", 1, speed);   
     }
@@ -63,24 +102,42 @@ public class Player : MonoBehaviour
         }
         else
         {
-            if (Input.GetKey(KeyCode.R))
+            foreach (Transform t in tail)
             {
-                foreach (Transform t in tail)
-                {
-                    Destroy(t.gameObject);
-                }
-                tail.Clear();
-
-                transform.position = Vector3.zero;
-                dir = Vector2.zero;
-
-                dead = false;
+                Destroy(t.gameObject);
             }
+            tail.Clear();
+
+            Destroy(GameObject.FindGameObjectWithTag("Food"));
+
+            xCounter = 9;
+            yCounter = 15;
+            transform.position = new Vector3(validXLocs[xCounter], validYLocs[yCounter]);
+            dir = Vector2.zero;
+
+            setDirBools();
+            dead = false;
+            //if (Input.GetKey(KeyCode.R))
+            //{
+            //    foreach (Transform t in tail)
+            //    {
+            //        Destroy(t.gameObject);
+            //    }
+            //    tail.Clear();
+
+            //    xCounter = 9;
+            //    yCounter = 15;
+            //    transform.position = new Vector3(validXLocs[xCounter], validYLocs[yCounter]);
+            //    dir = Vector2.zero;
+
+            //    setDirBools();
+            //    dead = false;
+            //}
         }
         
     }
 
-    private void setDirBoolToTrueOthersToFalse()
+    private void setDirBools()
     {
         if (dir == right)
         {
@@ -110,6 +167,13 @@ public class Player : MonoBehaviour
             lefting = false;
             uping = true;
         }
+        else if (dead)
+        {
+            righting = false;
+            downing = false;
+            lefting = false;
+            uping = false;
+        }
     }
 
     private void Move()
@@ -117,10 +181,42 @@ public class Player : MonoBehaviour
         if (!dead)
         {
             // Save current position
-            Vector2 pos = transform.position;
+            Vector2 pos = new Vector2(validXLocs[xCounter], validYLocs[yCounter]);
             
-            transform.Translate(dir);
-            setDirBoolToTrueOthersToFalse();
+            
+            //transform.Translate(dir);
+            //transform.position += dir;
+            //if (transform.position.x > 2.6 || transform.position.x < -2.6 || transform.position.y > 3.5 || transform.position.y < -4.7)
+            //    dead = true;
+
+            setDirBools();
+
+            if (dir == right)
+            {
+                xCounter++;
+            }
+            else if (dir == down)
+            {
+                yCounter--;
+            }
+            else if (dir == left)
+            {
+                xCounter--;
+            }
+            else if (dir == up)
+            {
+                yCounter++;
+            }
+
+            if (xCounter >= 0 && xCounter <= 18 && yCounter >= 0 && yCounter <= 29)
+            {
+                transform.position = new Vector3(validXLocs[xCounter], validYLocs[yCounter]);
+            }
+            else
+            {
+                dead = true;
+            }
+
 
             if (eat)
             {
--- Assets/Scripts/SpawnFood.cs
+++ Assets/Scripts/SpawnFood.cs
@@ -10,9 +10,35 @@ public class SpawnFood : MonoBehaviour
 
     private Vector3 loc;
 
+    //private float[] validXLocs = {2.511f, 2.232f, 1.953f, 1.674f, 1.395f, 1.116f, 0.837f, 0.558f, 0.279f, 0,
+    //                                -2.511f, -2.232f, -1.953f, -1.674f, -1.395f, -1.116f, -0.837f, -0.558f, -0.279f};
+    //private float[] validYLocs = {3.431f, 3.152f, 2.873f, 2.315f, 2.036f, 1.757f, 1.478f, 1.199f, 0.92f, 0.641f, 0.362f, 0.083f,
+    //                                -0.196f, -0.475f, -0.754f, -1.033f, -1.312f, -1.591f, -1.87f, -2.149f, -2.428f, -2.707f, -2.986f,
+    //                                -3.265f, -3.544f, -3.823f, -4.102f, -4.381f, -4.66f};
+
+    private float[] validXLocs = new float[19];
+    private float[] validYLocs = new float[30];
+
     // Start is called before the first frame update
     void Start()
     {
+        float minX = -2.511f;
+        validXLocs[0] = minX;
+        for (int i = 1; i < validXLocs.Length; i++)
+        {
+            minX += Player.moveDistance;
+            validXLocs[i] = minX;
+        }
+
+        float minY = -4.66f;
+        validYLocs[0] = minY;
+        for (int i = 1; i < validYLocs.Length; i++)
+        {
+            minY += Player.moveDistance;
+            validYLocs[i] = minY;
+        }
+
+
         Spawn();
     }
 
@@ -25,12 +51,58 @@ public class SpawnFood : MonoBehaviour
         }
     }
 
+    private float[] getCoods()
+    {
+        float[] coods = new float[2];
+
+        float x = Random.Range(-2.6f, 2.6f);
+        float y = Random.Range(-4.7f, 3.5f);
+        //Debug.Log("Random X: "+x+" Random Y: "+ y);
+
+        float difference = 0;
+        float smallestDifference = 10000;
+        int locOfSmallestDifference = 0;
+
+        for (int i = 0; i < validXLocs.Length; i++)
+        {
+            difference = Mathf.Abs(validXLocs[i] - x);
+            if (difference < smallestDifference)
+            {
+                smallestDifference = difference;
+                locOfSmallestDifference = i;
+            }
+        }
+        x = validXLocs[locOfSmallestDifference];
+
+        // reset smallest difference
+        smallestDifference = 10000;
+
+        for (int i = 0; i < validYLocs.Length; i++)
+        {
+            difference = Mathf.Abs(validYLocs[i] - y);
+            //Debug.Log("Diff: "+difference);
+            if (difference < smallestDifference)
+            {
+                smallestDifference = difference;
+                locOfSmallestDifference = i;
+            }
+        }
+        y = validYLocs[locOfSmallestDifference];
+
+        coods[0] = x;
+        coods[1] = y;
+
+
+        return coods;
+
+    }
+
     private void Spawn()
     {
-       
 
+        float[] coods = getCoods();
 
-        loc = new Vector3(Random.Range(-2.6f, 2.6f), Random.Range(-4.8f, 4.8f), 0);
+        loc = new Vector3(coods[0], coods[1], 0);
         
         fiq = Instantiate(food, loc, Quaternion.identity);
     }
